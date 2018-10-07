a3f91e0aa78eaa5752449678e6579894a6cfcf54
Syed Ahmed <sahmed@flyht.com>
2018-10-07 00:44:09 -0600

    first

---
+++ Helo/src/test/java/com/flyht/alpha/Helo/testhelo.java
@@ -0,0 +1,11 @@
+package com.flyht.alpha.Helo;
+
+public class testhelo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Helo helo testing testing");
+	}
+
+}
